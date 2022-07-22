7559654b64b09f54ca231a77f69da02cf6ca95f2
Utkarsh Upadhyay <utkarshupadhyaythgr8@gmail.com>
2022-07-22 17:21:06 +0530

    Updated PHP Emails

--- formhandler.php
+++ formhandler.php
@@ -3,13 +3,13 @@
     $visitor_email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
-    $email_from = 'email@example.com';
+    $email_from = 'from@email.com';
     $email_subject = 'New Form Submission From Generic Website';
     $email_body =   "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                     "Subject: $subject.\n".
                     "Message: $message.\n";
-    $to = 'utkarshupadhyaythgr8@gmail.com';
+    $to = 'to@email.com';
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to, $email_subject, $email_body, $headers);
